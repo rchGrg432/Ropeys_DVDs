dc41b5ca69935e2bfc652a7c3c2a624e2cc27874
Dell <gurungricha265@gmail.com>
2022-05-08 12:03:37 +0545

    Add project files.

---
+++ Common.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Common.aspx.cs" Inherits="Ropeys_DVDs.Common" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="tbSurname" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="AllDVDB" runat="server" OnClick="AllDVDB_Click" Text="All DVD" />
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnAvailableDVD" runat="server" Text="Available DVDs" />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Common.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ropeys_DVDs
+{
+    public partial class Common : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void AllDVDB_Click(object sender, EventArgs e)
+        {
+            string conStr = @"Data Source = .\SQLEXPRESS;" +
+                "Database = CourseworkDatabase;" +
+                "Trusted_Connection = True;";
+            String surname = tbSurname.Text;
+
+            //string str = ConfigurationManager.ConnectionStrings[conStr].ConnectionString;
+            SqlConnection con = new SqlConnection(conStr);
+            con.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select DVDTitle.DVDNumber, DVDTitle.DVDTitle, DVDTitle.DateReleased from DVDTitle inner join CastMember on DVDTitle.DVDNumber = CastMember.DVDNumber inner join Actor on CastMember.ActorNumber = actor.actor where Actor.ActorSurname Like '%" + tbSurname.Text + "%'", con);
+            DataTable dataTable = new DataTable();
+            sda.Fill(dataTable);
+
+            GridView1.DataSourceID = "";
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+            con.Close();
+        }
+    }
+}
